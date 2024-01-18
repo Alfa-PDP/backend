e442fa9b30c4540fb626ca082d1ed726af989f31
Mikhail Spiridonov <mspiridonov2706@gmail.com>
2024-01-18 22:45:30 +0100

    fix: change role from enum to Text;

--- src/database/models/user.py
+++ src/database/models/user.py
@@ -1,16 +1,8 @@
-from enum import StrEnum, auto
-
-from sqlalchemy import String
-from sqlalchemy.dialects.postgresql import ENUM
+from sqlalchemy import Text
 from sqlalchemy.orm import Mapped
 
 from database.models.declarative_base import BaseModel, Column
 from database.models.mixins import IdMixin, TsMixinCreated, TsMixinUpdated
-
-
-class UserRole(StrEnum):
-    employee = auto()
-    supervisor = auto()
 
 
 class User(BaseModel, IdMixin, TsMixinCreated, TsMixinUpdated):
@@ -18,18 +10,11 @@ class User(BaseModel, IdMixin, TsMixinCreated, TsMixinUpdated):
 
     __tablename__ = "users"
 
-    name: Mapped[str] = Column(String(64), nullable=False, comment="Имя пользователя")
-    family_name: Mapped[str] = Column(String(64), nullable=False, comment="Фамилия пользователя")
-    surname: Mapped[str] = Column(String(64), nullable=False, comment="Отчество пользователя")
-    position: Mapped[str] = Column(String(64), nullable=False, comment="Должность пользователя")
-
-    role: Mapped[str] = Column(
-        ENUM(UserRole, name="user_role"),
-        nullable=False,
-        comment="Роль пользователя",
-        default=UserRole.employee,
-        server_default=UserRole.employee,
-    )
+    name: Mapped[str] = Column(Text, nullable=False, comment="Имя пользователя")
+    family_name: Mapped[str] = Column(Text, nullable=False, comment="Фамилия пользователя")
+    surname: Mapped[str] = Column(Text, nullable=False, comment="Отчество пользователя")
+    position: Mapped[str] = Column(Text, nullable=False, comment="Должность пользователя")
+    role: Mapped[str] = Column(Text, nullable=False, comment="Роль пользователя")
 
     def __repr__(self) -> str:
-        return f"User({self.id}, {self.name}, {self.family_name}, {self.role}, {self.is_superuser})"
+        return f"User({self.id}, {self.name}, {self.family_name}, {self.role})"
