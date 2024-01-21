ba71ed281ba301d091709521eab24fc7be394a1a
Mikhail Spiridonov <mspiridonov2706@gmail.com>
2024-01-21 19:53:48 +0100

    chore: add team model and update user model;

---
+++ src/database/models/team.py
@@ -0,0 +1,25 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import UUID, ForeignKey, String
+from sqlalchemy.orm import Mapped, relationship
+
+from database.models.declarative_base import BaseModel, Column
+from database.models.mixins import IdMixin, TsMixinCreated, TsMixinUpdated
+
+if TYPE_CHECKING:
+    from database.models.user import User
+
+
+class Team(BaseModel, IdMixin, TsMixinCreated, TsMixinUpdated):
+    __tablename__ = "teams"
+
+    name: Mapped[str] = Column(String(100), nullable=False, comment="Наименование команды")
+    leader: Mapped[UUID] = Column(
+        ForeignKey("users.id", ondelete="RESTRICT", onupdate="RESTRICT"),
+        comment="Порядковый номер сотрудника в списке команды, который назначен руководителем команды",
+    )
+
+    members: Mapped["User"] = relationship(back_populates="team")
+
+    def __repr__(self) -> str:
+        return f"Team({self.id}, {self.name}, {self.leader})"
