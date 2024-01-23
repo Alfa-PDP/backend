848c0fce13a0b4e2bd718a3e4c1c6ac443898739
Pavel Ermeev <dobravam1@gmail.com>
2024-01-23 12:05:02 +0300

    fix task and tasknumber, create comment model

--- src/database/models/tasknumber.py
+++ src/database/models/tasknumber.py
@@ -4,24 +4,25 @@ from sqlalchemy import ForeignKey, UUID
 from sqlalchemy.orm import Mapped, relationship
 
 from database.models.declarative_base import BaseModel, Column
-from database.models.mixins import IdMixin
 
 if TYPE_CHECKING:
     from database.models.task import Task
     # from database.models.idp import Idp
 
 
-class Tasknumber(BaseModel, IdMixin):
+class TaskNumber(BaseModel):
     __tablename__ = "task_number"
 
     idp_id: Mapped[UUID] = Column(
-        ForeignKey("idp.id", ondelete="RESTRICT", onupdate="RESTRICT"), comment="ИПР связанный с задачей"
+        ForeignKey("idp.id", name="task_in_idp", ondelete="RESTRICT", onupdate="RESTRICT"),
+        comment="ИПР связанный с задачей",
     )
     task_id: Mapped[UUID] = Column(
-        ForeignKey("tasks.id", ondelete="RESTRICT", onupdate="RESTRICT"), comment="Порядковый номер в списке задач"
+        ForeignKey("tasks.id", name="task_id_list", ondelete="RESTRICT", onupdate="RESTRICT"),
+        comment="Порядковый номер в списке задач",
     )
 
     tasks: Mapped[list["Task"]] = relationship(back_populates="list_of_tasks")
 
     def __repr__(self) -> str:
-        return f"Tasknumber({self.id}, {self.idp_id}, {self.task_id})"
+        return f"TaskNumber({self.id}, {self.idp_id}, {self.task_id})"
