f5b4391faf82a1d7f70ce7f6fd82505c67af0400
Pavel Ermeev <dobravam1@gmail.com>
2024-01-23 12:05:02 +0300

    fix task and tasknumber, create comment model

---
+++ src/database/models/comment.py
@@ -0,0 +1,29 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey, String, UUID
+from sqlalchemy.orm import Mapped, relationship
+
+from database.models.declarative_base import BaseModel, Column
+from database.models.mixins import IdMixin, TsMixinCreated, TsMixinUpdated
+
+if TYPE_CHECKING:
+    from database.models.task import Task
+
+
+class Comment(BaseModel, IdMixin, TsMixinCreated, TsMixinUpdated):
+    __tablename__ = "comments"
+
+    user_id: Mapped[UUID] = Column(
+        ForeignKey("user.id", name="user_comment", ondelete="RESTRICT", onupdate="RESTRICT"),
+        comment="Комментарий от пользователя",
+    )
+    task_id: Mapped[UUID] = Column(
+        ForeignKey("task.id", name="task_comment", ondelete="RESTRICT", onupdate="RESTRICT"),
+        comment="Комментарий задачи",
+    )
+    text: Mapped[str] = Column(String(1000), nullable=False, comment="Комментарий задачи")
+
+    list_of_comments: Mapped["Task"] = relationship(back_populates="comments")
+
+    def __repr__(self) -> str:
+        return f"Comment({self.user_id}, {self.task_id}, {self.text})"
--- src/database/models/task.py
+++ src/database/models/task.py
@@ -8,27 +8,23 @@ from database.models.declarative_base import BaseModel, Column
 from database.models.mixins import IdMixin, TsMixinCreated, TsMixinUpdated
 
 if TYPE_CHECKING:
-    from database.models.tasknumber import Tasknumber
+    from database.models.tasknumber import TaskNumber
+    from database.models.comment import Comment
 
 
 class Task(BaseModel, IdMixin, TsMixinCreated, TsMixinUpdated):
     __tablename__ = "tasks"
 
     name: Mapped[str] = Column(String(100), nullable=False, comment="Наименование задачи")
-    description: Mapped[str] = Column(String, nullable=False, comment="Подробности задачи")
+    description: Mapped[str] = Column(String(1000), nullable=False, comment="Подробности задачи")
     task_type: Mapped[str] = Column(String(20), nullable=False, comment="Тип задачи")
     start_time: Mapped[datetime] = Column(Date, nullable=False, comment="Дата начала")
     end_time: Mapped[datetime] = Column(Date, nullable=False, comment="Дата окончания")
     status_description: Mapped[str] = Column(String(20), nullable=False, comment="Статус выполнения задачи")
-    employee_comment: Mapped[str] = Column(String, comment="Комментарий сотрудника")
-    boss_comment: Mapped[str] = Column(String, comment="Комментарий руководтеля")
-    importance: Mapped[str] = Column(String(2), nullable=False, comment="Значимость задачи")
-    task_id: Mapped[UUID] = Column(
-        ForeignKey("task_number.id", ondelete="RESTRICT", onupdate="RESTRICT"),
-        comment="Порядковый номер в списке задач",
-    )
+    importance: Mapped[str] = Column(String(10), nullable=False, comment="Значимость задачи")
 
-    list_of_tasks: Mapped["Tasknumber"] = relationship(back_populates="tasks")
+    comments: Mapped[list["Comment"]] = relationship(back_populates="list_of_comments")
+    list_of_tasks: Mapped["TaskNumber"] = relationship(back_populates="tasks")
 
     def __repr__(self) -> str:
-        return f"Task({self.id},{self.task_name})"
+        return f"Task({self.id}, {self.task_name})"
