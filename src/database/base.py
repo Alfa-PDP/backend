b13ee8e42b588df9c1bdd2e9a509e6343f2ec0ac
Mikhail Spiridonov <mspiridonov2706@gmail.com>
2024-01-25 17:25:59 +0100

    fix: add UserTeam model;

--- src/database/base.py
+++ src/database/base.py
@@ -1,3 +1,4 @@
 from database.models.declarative_base import BaseModel
 from database.models.team import Team
 from database.models.user import User
+from database.models.user_team import UserTeam
--- src/database/models/user.py
+++ src/database/models/user.py
@@ -1,13 +1,8 @@
-from typing import TYPE_CHECKING
-
-from sqlalchemy import UUID, ForeignKey, String
-from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
 
 from database.models.declarative_base import BaseModel, Column
 from database.models.mixins import IdMixin, TsMixinCreated, TsMixinUpdated
-
-if TYPE_CHECKING:
-    from database.models.team import Team
 
 
 class User(BaseModel, IdMixin, TsMixinCreated, TsMixinUpdated):
@@ -17,12 +12,6 @@ class User(BaseModel, IdMixin, TsMixinCreated, TsMixinUpdated):
     family_name: Mapped[str] = Column(String(50), nullable=False, comment="Фамилия пользователя")
     middle_name: Mapped[str] = Column(String(50), nullable=False, comment="Отчество пользователя")
     position: Mapped[str] = Column(String(100), nullable=False, comment="Должность пользователя")
-    role: Mapped[str] = Column(String(50), nullable=False, comment="Роль пользователя")
-    team_id: Mapped[UUID] = Column(
-        ForeignKey("teams.id", ondelete="RESTRICT", onupdate="RESTRICT", name="users_team_id_fkey")
-    )
-
-    team: Mapped["Team"] = relationship(back_populates="members")
 
     def __repr__(self) -> str:
         return f"User({self.id}, {self.name}, {self.family_name}, {self.role})"
