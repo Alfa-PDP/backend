47dff5f3cf144fd0df71afe385a443539e4c5aad
Mikhail Spiridonov <mspiridonov2706@gmail.com>
2024-01-29 17:51:13 +0100

    fix: idp.user_id comment;

--- src/database/models/idp.py
+++ src/database/models/idp.py
@@ -10,13 +10,14 @@ from database.models.mixins import IdMixin, TsMixinCreated, TsMixinUpdated
 class Idp(BaseModel, IdMixin, TsMixinCreated, TsMixinUpdated):
     __tablename__ = "idp"
 
-    user_id: Mapped[UUID] = Column(
-        ForeignKey("users.id", name="idp_users_fkey", ondelete="RESTRICT", onupdate="RESTRICT"),
-        comment="Статус выполнения задачи",
-    )
     start_date: Mapped[date] = Column(Date, nullable=False, comment="Начало ИПР")
     end_date: Mapped[date] = Column(Date, nullable=False, comment="Окончание ИПР")
     year: Mapped[int] = Column(Integer, nullable=False, comment="Год ИПР")
+
+    user_id: Mapped[UUID] = Column(
+        ForeignKey("users.id", name="idp_users_fkey", ondelete="RESTRICT", onupdate="RESTRICT"),
+        comment="ID пользователя",
+    )
 
     def __repr__(self) -> str:
         return f"Idp({self.id}, {self.user_id})"
